d3602cb460caebf0ff3906b92e3c0480c4782578
ahmedMeatmed <ahmedelazzba@gmail.com>
2026-06-07 00:51:16 +0300

    add/endpoints auth admin

--- Modules/Admin/routes/api.php
+++ Modules/Admin/routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use Modules\Admin\Http\Controllers\AdminController;
+use Modules\Admin\Http\Controllers\Api\V1\Auth\AuthController;
 
 Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('admins', AdminController::class)->names('admin');
+    // Route::apiResource('admins', AdminController::class)->names('admin');
+
+    Route::post('admin/login',[AuthController::class, 'login']);
+    Route::post('admin/logout',[AuthController::class, 'logout']);
 });
